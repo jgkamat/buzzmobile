dae309c6b11778c8e02642113171833eb8276db5
diptodip <dr.diptodip@gmail.com>
2016-04-12 19:05:15 -0400

    Semi-working 2: electric boogalo

--- buzzmobile/sense/image_const/image_const.cpp
+++ buzzmobile/sense/image_const/image_const.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
+#include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
 
@@ -7,7 +8,8 @@ int main(int argc, char** argv)
 {
   ros::init(argc, argv, "image_const");
   ros::NodeHandle nh;
-  ros::Publisher pub = nh.advertise<sensor_msgs::Image>("image", 1);
+  image_transport::ImageTransport it(nh);
+  image_transport::Publisher pub = it.advertise("/static_image", 1);
   cv::Mat image = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
   sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
 
