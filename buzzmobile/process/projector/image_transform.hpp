04d46d5c0e74969262967756d10385a294159c7c
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2016-08-19 18:29:29 -0400

    properly project live cam messages

--- buzzmobile/process/projector/image_transform.hpp
+++ buzzmobile/process/projector/image_transform.hpp
@@ -9,25 +9,27 @@ namespace image_transform {
 // The width and height of the final image. Will depend on the size of the
 // initial image.
 // TODO: modify to fit the car's image dimensions.
-int persp_transform_width = 3700;
-int persp_transform_height = 6500;
+int persp_transform_width = 480;
+int persp_transform_height = 640;
 
 // The coords of the edges of a chessboard lying on the ground,
 // in a training image.
+// These coords work for when the camera is at exactly 42.25 inches above
+// ground, at a slight angle downwards (you can open rviz to title the angle).
 cv::Point2f persp_transform_src_pts[4] = {
-  cv::Point2f(1593,2168),
-  cv::Point2f(1850,2168),
-  cv::Point2f(1859,2345),
-  cv::Point2f(1577,2345)
+  cv::Point2f(307,350),
+  cv::Point2f(342,350),
+  cv::Point2f(343,373),
+  cv::Point2f(303,373)
 };
 
 // The points where each of the above will be projected onto.
 // In our case, in the middle wrt x, and close to bottom wrt y.
 cv::Point2f persp_transform_dst_pts[4] = {
-  cv::Point2f(-20+(persp_transform_width/2),persp_transform_height-50),
-  cv::Point2f(20+(persp_transform_width/2),persp_transform_height-50),
-  cv::Point2f(20+(persp_transform_width/2),persp_transform_height-10),
-  cv::Point2f(-20+(persp_transform_width/2),persp_transform_height-10)
+  cv::Point2f(-10+(persp_transform_width/2),persp_transform_height-20-30),
+  cv::Point2f(10+(persp_transform_width/2),persp_transform_height-20-30),
+  cv::Point2f(10+(persp_transform_width/2),persp_transform_height-30),
+  cv::Point2f(-10+(persp_transform_width/2),persp_transform_height-30)
 };
 
 cv::Mat transform = getPerspectiveTransform(persp_transform_src_pts,
