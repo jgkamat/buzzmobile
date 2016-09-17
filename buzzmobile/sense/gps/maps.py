f56dc71e616851fe19041f4094befee391fdc120
Diptodip Deb <dr.diptodip@gmail.com>
2016-09-16 23:54:24 -0400

    Haversine.

--- buzzmobile/sense/gps/maps.py
+++ buzzmobile/sense/gps/maps.py
@@ -48,3 +48,14 @@ def get_points(start, destination, top_left=None, width_height=None):
 
     #get the list of lat-long coordinates from the directions
     points = pl.decode(polyline)
+
+def haversine(lat1, lon1, lat2, lon2):
+    delta_lat = radians(lat2 - lat1)
+    delta_lon = radians(lon2 - lon1)
+    lat1 = radians(lat1)
+    lat2 = radians(lat2)
+    a = sin(delta_lat / 2)**2 + cos(lat1) * cos(lat2) * sin(delta_lon/2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    R = 6371e3
+    d = R * c
+    return d
