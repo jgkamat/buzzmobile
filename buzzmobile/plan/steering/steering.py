cd83315099f781c6824b9e0fcc7e6c4da6cc58cd
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2016-09-17 11:29:18 -0400

    add somewhat functional version of steering, lacks negative angles, and cv drawing

--- buzzmobile/plan/steering/steering.py
+++ buzzmobile/plan/steering/steering.py
@@ -1,18 +1,11 @@
+from __future__ import division
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 # TODO: REMOVE, use ROS node message.
 def merge_frames(frames, weights):
-    """
-    Computes a weighted average of the given frames.
-
-    Args:
-        frames: a list of cv2 matrices to be merged
-        weights: the weight each frame should have in the output
-
-    Returns:
-        A merged version of the frames
-    """
     total_weight = sum(weights)
 
     height, width = frames[0].shape
@@ -26,7 +19,7 @@ def merge_frames(frames, weights):
 # END REMOVE
 
 
-PIXELS_PER_METER = 30 # TODO: MODIFY THIS ONCE IMAGE IS FINAL
+PIXELS_PER_METER = 15 # TODO: MODIFY THIS ONCE IMAGE IS FINAL
 
 def turning_radius(steering_angle, wheel_base=3):
     """
@@ -37,28 +30,31 @@ def turning_radius(steering_angle, wheel_base=3):
         return float('inf')
     return abs(wheel_base / np.tan(steering_angle))
 
-def project_tentacle(x_0, y_0, steering_angle, travel_distance, num_points,
-        wheel_base=3):
+def project_tentacle(x_0, y_0, heading, steering_angle, travel_distance,
+        num_points, wheel_base=3):
     """
     Returns a list of expected positions (in pixel coordinates),
     starting at (x, y), given constant steering_angle (in radians) and
-    travel_distance (in meters).
+    travel_distance (in meters). Heading is angle at which the car is facing.
     """
     if steering_angle == 0:
         y = y_0 + (PIXELS_PER_METER * travel_distance)
-        return [(int(x_0), int(y))] + project_tentacle(x_0, y, steering_angle,
+        if num_points == 0:
+            return [(x_0, y)]
+        return [(x_0, y)] + project_tentacle(x_0, y, heading, steering_angle,
                 travel_distance, num_points - 1, wheel_base)
 
     radius = turning_radius(steering_angle, wheel_base)
-    travel_angle = travel_distance / radius
+    travel_angle = travel_distance / radius + heading
 
-    x = PIXELS_PER_METER * radius * np.sin(travel_angle)
-    y = PIXELS_PER_METER * radius * (1 - np.cos(travel_angle))
+    x = x_0 + PIXELS_PER_METER * radius * (np.cos(heading) - np.cos(travel_angle))
+    y = y_0 + PIXELS_PER_METER * radius * (np.sin(travel_angle) - np.sin(heading))
 
     if num_points == 0:
-        return [(int(x), int(y))]
-    return [(int(x), int(y))] + project_tentacle(x, y, steering_angle * 2,
-            travel_distance, num_points - 1, wheel_base)
+        return [(x, y)]
+    return [(x, y)] + project_tentacle(x, y,
+                travel_angle, steering_angle, travel_distance,
+                num_points - 1, wheel_base)
 
 
 if __name__ == '__main__':
@@ -67,21 +63,33 @@ if __name__ == '__main__':
     height = width = 500
 
     frame1 = np.zeros((height, width), np.uint8)
-    cv2.line(frame1, (width/2, height), (width/2, height/2), 255, width/6)
-    cv2.line(frame1, (width/2, height/2), (width, height/2), 255, width/6)
+    cv2.line(frame1, (width//2, height), (width//2, height//2), 255, width//6)
+    cv2.line(frame1, (width//2, height//2), (width, height//2), 255, width//6)
 
     frame2 = np.zeros((height, width), np.uint8)
-    cv2.circle(frame2, (width/2, height), int(width//2.5), 255, thickness=-1)
+    cv2.circle(frame2, (width//2, height), int(width//2.5), 255, thickness=-1)
 
     result_frame = merge_frames([frame1, frame2], [1.0, 1.0])
 
-    points = project_tentacle(0, 0, 0.1, 1, 15)
-    print points
-    for i in range(len(points) - 1):
-        pt1 = points[i]
-        pt2 = points[i+1]
-        cv2.line(result_frame, pt1, pt2, [0, 150, 136], 4)
+    max_angle = 0.5
+    travel_distance = 0.1
+    angles = np.linspace(0.0, max_angle, max_angle * 100)
+    colors = cm.rainbow(np.linspace(0, 1, len(angles)))
 
-    cv2.imshow('result_frame', result_frame)
-    cv2.waitKey(0)
+    for angle, c in zip(angles, colors):
+        pos_points = project_tentacle(0, 0, 0, angle, travel_distance, 30)
+        neg_points = project_tentacle(0, 0, 0, -angle, travel_distance, 30)
+
+        plt.scatter(*zip(*pos_points), color=c)
+        plt.scatter(*zip(*neg_points), color=c)
+
+    plt.show()
+
+    # for i in range(len(points) - 1):
+        # pt1 = points[i]
+        # pt2 = points[i+1]
+        # cv2.line(result_frame, pt1, pt2, [0, 150, 136], 4)
+
+    # cv2.imshow('result_frame', result_frame)
+    # cv2.waitKey(0)
     cv2.destroyAllWindows()
