fix normalization

buzzmobile/plan/steering/steering.py
@@ -69,9 +69,10 @@ def score_tentacle(points, frame):
         pt2 = points[i+1]
         cv2.line(tentacle_mask, pt1, pt2, [255, 255, 255], 1)
 
-    normalizing_factor = sum(sum(tentacle_mask))
-    if normalizing_factor == 0.0:
-        return 0.0
+    # normalizing_factor = sum(sum(tentacle_mask))
+    # if normalizing_factor == 0.0:
+        # return 0.0
+    normalizing_factor = WIDTH * 255 * 2
 
     tentacle_score_image = cv2.bitwise_and(frame, frame, mask=tentacle_mask)
     tentacle_score = sum(sum(tentacle_score_image)) / normalizing_factor
@@ -161,8 +162,8 @@ if __name__ == '__main__':
         return merged
 
     frame1 = np.zeros((HEIGHT, WIDTH), np.uint8)
-    cv2.line(frame1, (WIDTH//2, HEIGHT), (WIDTH//2, HEIGHT//2), 255, WIDTH//6)
-    cv2.line(frame1, (WIDTH//2, HEIGHT//2), (WIDTH, HEIGHT//2), 255, WIDTH//6)
+    cv2.line(frame1, (WIDTH//2, HEIGHT), (WIDTH//2, HEIGHT//2 + 150), 255, WIDTH//6)
+    cv2.line(frame1, (WIDTH//2, HEIGHT//2 + 150), (WIDTH, HEIGHT//2 + 150), 255, WIDTH//6)
 
     frame2 = np.zeros((HEIGHT, WIDTH), np.uint8)
     cv2.circle(frame2, (WIDTH//2, HEIGHT), int(WIDTH//2.5), 255, thickness=-1)