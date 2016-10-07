0042d21fb08266a26c4ce0854dfaefd0bccbcc9f
Diptodip Deb <dr.diptodip@gmail.com>
2016-10-07 04:23:17 -0400

    Now able to select angled rectangular subregion of path image.

--- buzzmobile/sense/gps/maps.py
+++ buzzmobile/sense/gps/maps.py
@@ -83,6 +83,6 @@ def haversine(lat1, lon1, lat2, lon2):
     lat2 = math.radians(lat2)
     a = math.sin(delta_lat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(delta_lon/2)**2
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-    R = 6371e3
+    R = 6371
     d = R * c
     return d
