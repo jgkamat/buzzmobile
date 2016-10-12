5012d4efcbac481615c864b0d553524c538e7a6b
Josh Morton <joshua.morton13@gmail.com>
2016-10-11 20:43:52 -0400

    Convert user input node to a generic repl
    This way we can have a single node that handles various forms of user input for any other parts of the system.

--- buzzmobile/sense/inputer/repl.py
+++ buzzmobile/sense/inputer/repl.py
@@ -3,12 +3,12 @@ import rospy
 from std_msgs.msg import String
 
 def input_node():
-    pub = rospy.Publisher('input_value', String, queue_size=0)
+    pub = rospy.Publisher('destination', String, queue_size=0)
     rospy.init_node('inputer', anonymous=True)
     # Publish a nonsense initial value to make sure subscribers don't explode
     pub.publish("")
     while not rospy.is_shutdown():
-        new_dst = raw_input("Dest> ")
+        new_dst = raw_input("Dest >> ")
         pub.publish(new_dst)
 
 if __name__ == '__main__':
