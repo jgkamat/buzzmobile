5c5a17c83033ff9a61ba4568b9feb42bf7325109
Kevin Berry <kpberry11@gmail.com>
2016-10-11 21:36:46 -0700

    Inline variables, adjust subscriber queue size to 1 upon ros's request

--- buzzmobile/sense/lidar/lidar.py
+++ buzzmobile/sense/lidar/lidar.py
@@ -9,17 +9,7 @@ import rospy
 
 import sys
 sys.path.append('../../include')
-import constants
-
-
-
-image_width_px = constants.image_height
-image_height_px = constants.image_height
-image_width_m = constants.image_width_m
-image_height_m = constants.image_height_m
-pixels_per_m = constants.pixels_per_m
-
-
+import constants as c
 
 lidar_publisher = rospy.Publisher('lidar_model', Image, queue_size=0)
 
@@ -50,16 +40,16 @@ def get_lidar_image_message(matrix):
 # points is a list of tuples. i.e. [(x0, y0), (x1, y1), ...]
 def gen_point_image(points):
     # Generate matrix of all 0's to represent image
-    matrix = np.zeros((image_height_px, image_width_px), np.uint8)
+    matrix = np.zeros((c.image_height, c.image_width), np.uint8)
     matrix.fill(255)
  
     # Remap points to from lidar coordinates to image coordinates
     remappedPoints = []
     for point in points:
-	x = point[0] * pixels_per_m
-        y = point[1] * pixels_per_m
-	newx = x + (image_width_px / 2)
-        newy = image_height_px - y
+	x = point[0] * c.pixels_per_m
+        y = point[1] * c.pixels_per_m
+	newx = x + (c.image_width / 2)
+        newy = c.image_height - y
         remappedPoints.append((newx, newy))
 
     remappedPoints = sorted(remappedPoints, key=lambda xy: xy[0])
@@ -69,8 +59,8 @@ def gen_point_image(points):
     
     remappedPoints.insert(0, (0,0))
     remappedPoints.insert(1, (0, minPoint[1]))
-    remappedPoints.append((image_width_px, maxPoint[1]))
-    remappedPoints.append((image_width_px, 0))
+    remappedPoints.append((c.image_width, maxPoint[1]))
+    remappedPoints.append((c.image_width, 0))
 
     #remappedPoints = sorted(remappedPoints, key=lambda xy: xy[0])
 
@@ -88,7 +78,7 @@ def gen_point_image(points):
     return matrix
 
 
-#genLidarImage([(1, 2.4), (2, 3.3), (1.5, 9), (3.5, 3.6)])
+#gen_point_image([(1, 2.4), (2, 3.3), (1.5, 9), (3.5, 3.6)])
 
 def lidar_node():
     rospy.init_node('lidar_node', anonymous=True)
