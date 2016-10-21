b2fb3d67499106102f086613f6a956dabc5dfbf8
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2016-10-21 15:49:42 -0400

    a few fixes and factorings in car_pose_mux

--- buzzmobile/plan/car_pose_mux/car_pose_mux.py
+++ buzzmobile/plan/car_pose_mux/car_pose_mux.py
@@ -1,46 +1,46 @@
 #!/usr/bin/env python
 import rospy
+
 from buzzmobile.msg import CarPose
 from buzzmobile.msg import CarState
 
-#Global Variables
-controller_car_pose = None
-auto_car_pose = None
-curr_car_state = None
+
+# Global Variables
+state = {
+        'manual_car_pose': None,
+        'auto_car_pose': None,
+        'curr_car_state': CarState.START}
 
 car_pose_pub = rospy.Publisher('car_pose', CarPose, queue_size=0)
 
-def publish():
-    if curr_car_state != None: 
-        car_pose = CarPose()
-        if (curr_car_state.state == CarState.AUTO):
-            car_pose = auto_car_pose
-        else:
-            car_pose = controller_car_pose
-        
-        if (car_pose != None):
-            rospy.loginfo("Car in " + ("AUTO" if curr_car_state.state == CarState.AUTO else "MANUAL") + " mode.")
-            car_pose_pub.publish(car_pose)
+def mux(car_state):
+    if car_state == CarState.AUTO: return state['auto_car_pose']
+    if car_state == CarState.MANUAL: return state['manual_car_pose']
 
-def set_controller_car_pose(car_pose):
-    global controller_car_pose
-    controller_car_pose = car_pose
+def publish():
+    car_pose = mux(state['curr_car_state'])
+    if car_pose is not None:
+        car_pose_pub.publish(car_pose)
+
+def set_manual_car_pose(car_pose):
+    state['manual_car_pose'] = car_pose
+    publish()
 
 def set_auto_car_pose(car_pose):
-    global auto_car_pose
-    auto_car_pose = car_pose
+    state['auto_car_pose'] = car_pose
+    publish()
 
 def set_car_state(car_state):
-    global curr_car_state
-    curr_car_state = car_state
+    state['curr_car_state'] = car_state
+    rospy.loginfo("Car in " + (
+        "AUTO" if car_state == CarState.AUTO else "MANUAL") + " mode.")
     publish()
 
 def mux_node():
     rospy.init_node('car_pos_mux', anonymous=True)
-    rospy.Subscriber('controller_car_pose', CarPose, set_controller_car_pose)
-    #rospy.Subscriber('auto_car_pose', CarPose, set_auto_car_pose)
+    rospy.Subscriber('manual_car_pose', CarPose, set_manual_car_pose)
+    rospy.Subscriber('auto_car_pose', CarPose, set_auto_car_pose)
     rospy.Subscriber('car_state', CarState, set_car_state)
     rospy.spin()
 
-if __name__ == '__main__':
-    mux_node()
+if __name__ == '__main__': mux_node()
