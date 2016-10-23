601bd8403d1ac1a99a2dc144d85d1b3b91131c89
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2016-10-23 15:47:47 -0400

    factor out more things in lidar, handle NaNs, and make boundary points more intuitive and foolproof

--- buzzmobile/process/lidar_to_frame/lidar_to_frame.py
+++ buzzmobile/process/lidar_to_frame/lidar_to_frame.py
@@ -7,7 +7,7 @@ import rospy
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
 from sensor_msgs.msg import LaserScan
-from math import sin, cos, pi
+from math import sin, cos, pi, isnan
 
 image_width = rospy.get_param("image_width")
 image_height = rospy.get_param("image_height")
@@ -39,9 +39,12 @@ def laser_scan_to_cartesian(laser_scan):
     angle = laser_scan.angle_min + pi / 2
     #convert points from polar to cartesian (origin at (width/2, height))
     for i in range(len(ranges)):
+        # when using hokuyo_node, far points are inf.
+        # when using urg_node, they're nan.
         distance_to_point = (
-                ranges[i] if ranges[i] != float('inf')
+                ranges[i] if ranges[i] != float('inf') and not isnan(ranges[i])
                 else image_width + image_height)
+
         lidar_points.append(
             (cos(angle) * distance_to_point * pixels_per_m + (image_width / 2),
              image_height - (sin(angle) * distance_to_point * pixels_per_m))
@@ -62,22 +65,13 @@ def gen_point_image(points):
     """ Generate a cv image from a list of tuple (x, y) coordinates """
     # Generate image of all 0's to represent image
     image = np.zeros((image_height, image_width), np.uint8)
-    image.fill(255)
-
-    #get the points at ~pi and ~0
-    min_point = points[0]
-    max_point = points[len(points) - 1]
 
     #add boundary points for line drawing
-    points.insert(0, (image_width, 0))
-    points.insert(1, (image_width, image_height + 1))
-    points.insert(2, (min_point[0], image_height))
-    points.append((max_point[0], image_height))
-    points.append((0, image_height + 1))
-    points.append((0, 0))
+    points.insert(0, (image_width//2, image_height))
+    points.append((image_width//2, image_height))
 
-    #fill the undrivable portion of the image with black
-    cv2.fillPoly(image, [np.array(points, np.int32)], 0)
+    #fill the drivable portion of the image with white
+    cv2.fillPoly(image, [np.array(points, np.int32)], 255)
     return image
 
 def lidar_node():
