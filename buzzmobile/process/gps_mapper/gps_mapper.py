2da14d620c19a31a26777cdea88435c1b53142e8
Diptodip Deb <dr.diptodip@gmail.com>
2016-10-25 20:06:16 -0400

    No longer using an object to store frame variables.

--- buzzmobile/process/gps_mapper/gps_mapper.py
+++ buzzmobile/process/gps_mapper/gps_mapper.py
@@ -4,21 +4,17 @@ import rospy
 import math
 import polyline as pl
 import interpolate
+from collections import namedtuple
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import NavSatFix
 from sensor_msgs.msg import Image
 from std_msgs.msg import Float64
 from std_msgs.msg import String
 
-class Frames(object):
-    def __init__(self):
-        bearing = None
-        points = None
-        location = None
-
 #Global Variables
 bridge = CvBridge()
-frames = Frames()
+frames = {}
+frames['bearing'] = frames['points'] = frames['location'] = None
 gps_model_pub = rospy.Publisher('gps_model', Image, queue_size=0)
 x_scale = y_scale = 1000 * rospy.get_param('pixels_per_m')
 y_range = x_range = 0
@@ -26,33 +22,33 @@ y_range = x_range = 0
 def set_points(polyline):
     if polyline is not None:
         points = pl.decode(polyline.data)
-        y_range, x_range, top_left, bottom_right = interpolate.dimensions(frames.points)
-        frames.points = [(y, -x) for (x, y) in points]
-        _, _, top_left, bottom_right = interpolate.dimensions(frames.points)
+        y_range, x_range, top_left, bottom_right = interpolate.dimensions(frames['points'])
+        frames['points'] = [(y, -x) for (x, y) in points]
+        _, _, top_left, bottom_right = interpolate.dimensions(frames['points'])
         height = y_range * y_scale
         width = x_range * x_scale
-        frames.points = interpolate.normalized_points(frames.points, int(round(width)), int(round(height)))
+        frames['points'] = interpolate.normalized_points(frames['points'], int(round(width)), int(round(height)))
 
 def update_image():
-    if hasattr(frames, 'points'):
-        _, _, top_left, bottom_right = interpolate.dimensions(frames.points)
+    if frames['points'] is not None:
+        _, _, top_left, bottom_right = interpolate.dimensions(frames['points'])
         height = int(round(y_range * y_scale))
         width = int(round(x_range * x_scale))
-        point = (frames.location[0], -frames.location[1])
+        point = (frames['location'][0], -frames['location'][1])
         if y_range is not 0:
             point = interpolate.normalize_single_point(y_range, x_range, height, width, top_left, bottom_right, point)
-        result = interpolate.xwindow(frames.points, point, frames.bearing, rospy.get_param('image_height'), rospy.get_param('image_width'))
+        result = interpolate.xwindow(frames['points'], point, frames['bearing'], rospy.get_param('image_height'), rospy.get_param('image_width'))
         result_msg = bridge.cv2_to_imgmsg(result, encoding='mono8')
         gps_model_pub.publish(result_msg)
 
 def set_bearing(angle):
-    frames.bearing = angle.data
-    if hasattr(frames, 'location'):
+    frames['bearing'] = angle.data
+    if frames['location'] is not None:
         update_image()
 
 def set_location(fix_location):
-    frames.location = (fix_location.latitude, fix_location.longitude)
-    if hasattr(frames, 'bearing'):
+    frames['location'] = (fix_location.latitude, fix_location.longitude)
+    if frames['bearing'] is not None:
         update_image()
 
 def gps_mapper_node():
