d29c6f9727a6be4bff9e763445456e9f15c9bacf
Diptodip Deb <dr.diptodip@gmail.com>
2016-11-07 17:24:53 -0500

    Convert to radians twice.

--- buzzmobile/process/gps_mapper/interpolate.py
+++ buzzmobile/process/gps_mapper/interpolate.py
@@ -29,10 +29,10 @@ def haversine(lat1, lon1, lat2, lon2):
     """
     Calculates the distance between two lat-lon points in kilometers.
     """
-    delta_lat = math.radians(lat2 - lat1)
-    delta_lon = math.radians(lon2 - lon1)
     lat1 = math.radians(lat1)
     lat2 = math.radians(lat2)
+    delta_lat = math.radians(lat2 - lat1)
+    delta_lon = math.radians(lon2 - lon1)
     a = (math.sin(delta_lat / 2)**2
         + math.cos(lat1) * math.cos(lat2) * math.sin(delta_lon/2)**2)
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
