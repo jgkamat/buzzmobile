40bcc91b766f55483ae0996c545683ce3e54558e
Diptodip Deb <dr.diptodip@gmail.com>
2016-11-07 17:33:59 -0500

    Fix radian conversions in haversine calculation.

--- buzzmobile/process/gps_mapper/interpolate.py
+++ buzzmobile/process/gps_mapper/interpolate.py
@@ -31,8 +31,10 @@ def haversine(lat1, lon1, lat2, lon2):
     """
     lat1 = math.radians(lat1)
     lat2 = math.radians(lat2)
-    delta_lat = math.radians(lat2 - lat1)
-    delta_lon = math.radians(lon2 - lon1)
+    lon1 = math.radians(lon1)
+    lon2 = math.radians(lon2)
+    delta_lat = (lat2 - lat1)
+    delta_lon = (lon2 - lon1)
     a = (math.sin(delta_lat / 2)**2
         + math.cos(lat1) * math.cos(lat2) * math.sin(delta_lon/2)**2)
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
