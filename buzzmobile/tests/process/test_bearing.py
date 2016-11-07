5a877a5045f5a40790f77d71d0ffead71cec9419
Joshua Morton <joshua.morton13@gmail.com>
2016-11-07 18:53:11 -0500

    Added basic tests for bearing

--- buzzmobile/tests/process/test_bearing.py
+++ buzzmobile/tests/process/test_bearing.py
@@ -1,13 +1,8 @@
 import unittest
 from tests import rostest_utils
-import rospy
 import numpy as np
 from collections import namedtuple
-
-# we need this set before importing bearing
-rospy.set_param('median_filter_size', 5)
-rospy.set_param('min_fix_distance', .01)
-from process.bearing import bearing
+from process.bearing import calculate_directions
 
 fix = namedtuple('fix', ['latitude', 'longitude'])
 
@@ -16,7 +11,7 @@ class TestBearing(unittest.TestCase):
     def test_distance(self):
         fix1 = fix(33.636700, -84.427863)
         fix2 = fix(39.029128, -111.838257)
-        assert np.isclose(bearing.get_distance(fix1, fix2), 2517000, rtol=.01)
+        assert np.isclose(calculate_directions.get_distance(fix1, fix2), 2517000, rtol=.01)
 
 
 @rostest_utils.with_roscore
