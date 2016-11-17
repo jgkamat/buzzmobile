f97b32f43d2fb165ece4200ae3d315479a6083fa
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2016-11-17 16:06:41 -0500

    pylint state_viz

--- buzzmobile/tools/state_viz/state_viz.py
+++ buzzmobile/tools/state_viz/state_viz.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
+"""state_viz: node that publishes current car state image for mission_control.
+
+Subscribes:
+    car_pose CarPose command which also stores car state (mode)
+Publishes:
+    state_viz Image representing current car state
+"""
+
 import cv2
 import os
 import rospy
-import sys
 
 from buzzmobile.msg import CarPose
 from cv_bridge import CvBridge
@@ -15,18 +22,21 @@ g['start'] = g['auto'] = g['manual'] = g['neutral'] = None
 pub = rospy.Publisher('car_state_image', Image, queue_size=1)
 
 def publish_car_state_image(car_pose):
+    """Publishes the correct state image given a car_pose.mode."""
     if car_pose.mode is not None and car_pose.mode in g.keys():
         pub.publish(g[car_pose.mode])
 
 def create_images():
-    BASE_PATH = os.path.dirname(os.path.realpath(__file__))
+    """Create the state images in memory for fast publishing."""
+    base_path = os.path.dirname(os.path.realpath(__file__))
     bridge = CvBridge()
 
     for image_name in ('start', 'auto', 'manual', 'neutral'):
-        img = cv2.imread(os.path.join(BASE_PATH, image_name + '.png'))
+        img = cv2.imread(os.path.join(base_path, image_name + '.png'))
         g[image_name] = bridge.cv2_to_imgmsg(img, "bgr8")
 
 def state_viz_node():
+    """Initializes state_viz node."""
     rospy.init_node('state_viz', anonymous=True)
     create_images()
     sleep(1)
