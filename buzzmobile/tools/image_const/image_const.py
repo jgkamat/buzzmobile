474f7945015d88df164b0c0f630fa377ed51749f
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2016-11-17 16:01:19 -0500

    pylint image_const

--- buzzmobile/tools/image_const/image_const.py
+++ buzzmobile/tools/image_const/image_const.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
+"""image_const: node that continuously publishes a given image from file.
+
+Publishes:
+    image_filename Image with topic name same as file name
+"""
+
+from __future__ import print_function
+
 import sys
 import rospy
 import cv2
@@ -10,11 +18,11 @@ from sensor_msgs.msg import Image
 bridge = CvBridge()
 
 
-def publish_image(image_filename):
-    imgFile = cv2.imread(image_filename)
-    gray_im = cv2.cvtColor(imgFile, cv2.COLOR_BGR2GRAY)
-    
-    imgMsg = bridge.cv2_to_imgmsg(gray_im, encoding="mono8")
+def continuously_publish_image(image_filename):
+    """Publishes the image given at a constant rate."""
+    cv2_img = cv2.imread(image_filename)
+    gray_im = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2GRAY)
+    img_msg = bridge.cv2_to_imgmsg(gray_im, encoding="mono8")
 
     topic_name = sys.argv[1].split('/')[-1][:-4]
     pub = rospy.Publisher(topic_name, Image, queue_size=1)
@@ -24,7 +32,7 @@ def publish_image(image_filename):
     rate = rospy.Rate(1) # 1hz
 
     while not rospy.is_shutdown():
-        pub.publish(imgMsg)
+        pub.publish(img_msg)
         rate.sleep()
 
 
@@ -32,5 +40,4 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("Please provide a image filename")
     else:
-        image_filename = sys.argv[1]
-        publish_image(image_filename)
+        continuously_publish_image(sys.argv[1])
