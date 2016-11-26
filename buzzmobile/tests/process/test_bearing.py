affaa67acfbbfde2b95bc5964369a8bc7bdae80f
Joshua Morton <joshua.morton13@gmail.com>
2016-11-26 01:38:42 -0500

    Add default callback function to mock nodes

--- buzzmobile/tests/process/test_bearing.py
+++ buzzmobile/tests/process/test_bearing.py
@@ -22,11 +22,8 @@ class TestBearingNode(unittest.TestCase):
     @with_launch_file('buzzmobile', 'test_params.launch')
     @launch_node('buzzmobile', 'bearing.py')
     def test_bearing_node(self):
-        def callback(s, data):
-            s.result = data.data
-
         with mock_pub('/fix', NavSatFix, queue_size=None) as fix_node:
-            with check_topic('/buzzmobile/bearing', Float64, callback) as ct:
+            with check_topic('/buzzmobile/bearing', Float64) as ct:
                 # send mock data
                 fix_node.send(NavSatFix(None, None, 33.636700, -84.427863, None, None, None))
                 fix_node.send(NavSatFix(None, None, 39.029128, -111.838257, None, None, None))
