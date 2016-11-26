46fe8abf47f2f086b666bae8dbba8cf086b2b4a1
Joshua Morton <joshua.morton13@gmail.com>
2016-11-26 01:27:16 -0500

    Added import to __init__.py in testing module

--- buzzmobile/tests/process/test_bearing.py
+++ buzzmobile/tests/process/test_bearing.py
@@ -1,7 +1,5 @@
 import unittest
-from tests.test_utils.rostest_utils import with_roscore
-from tests.test_utils.mock_nodes import mock_pub, check_topic
-from tests.test_utils.launch_tools import with_launch_file, launch_node
+from tests.test_utils import with_roscore, mock_pub, check_topic, with_launch_file, launch_node
 import numpy as np
 from collections import namedtuple
 from process.bearing import calculate_directions
