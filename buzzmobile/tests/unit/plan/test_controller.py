6a15f42ac6a891fa348817d3c920c758d37269ce
Cole Taylor <coletaylor788@gmail.com>
2017-02-13 19:20:28 -0800

    Adding test for car state. Currently inconsistent

--- buzzmobile/tests/unit/plan/test_controller.py
+++ buzzmobile/tests/unit/plan/test_controller.py
@@ -7,6 +7,8 @@ from sensor_msgs.msg import Joy
 from buzzmobile.msg import CarPose
 from buzzmobile.msg import CarState
 
+import time
+
 @with_roscore
 class TestCarState(unittest.TestCase):
 
@@ -20,9 +22,29 @@ class TestCarState(unittest.TestCase):
             # Even though there is an await in the bearing test.
             # Also, it doesn't matter which topic is checked
             with check_topic('/buzzmobile/car_state', CarState) as cs:
+               
+                # Note: You must specify arrays for axes and buttons for the
+                # Joy constructor with length at least 16. Otherwise it
+                # does not work. My guess is the controller node has an 
+                # index out of bounds exception but I haven not confirmed
+                axes = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+                buttons = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+                buttonsEmpty = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+                expectedOutcomes = [1]
+
                 # Ensure starts in Start Mode
-                assert True
-                # TODO(cole): finish writing test
-                # joy_node.send(Joy())
-                # await(cs.wait_for_message())
-                # assert cs.message.state == CarState.START
+                joy_node.send(Joy(None, axes, buttonsEmpty))
+                await(cs.wait_for_message())
+                assert cs.message.state == CarState.START
+                
+                #for val in expectedOutcomes:
+                joy_node.send(Joy(None, axes, buttons))
+                joy_node.send(Joy(None, axes, buttons))
+                joy_node.send(Joy(None, axes, buttonsEmpty))
+                #await(joy_node.wait_for_message())
+                #await(joy_node.wait_for_message())
+                await(cs.wait_for_message())
+                time.sleep(10)
+                assert cs.message.state == 1
+               
