354059f3c3e5ccf11b82930d5a8429668942ea07
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2017-02-16 20:43:19 -0800

    revert test

--- buzzmobile/tests/simulation/test_sanity.py
+++
@@ -1,9 +0,0 @@
-from tests.test_utils import RosTest, with_launch_file
-
-
-class TestBearingNode(RosTest):
-
-    @with_launch_file('buzzmobile', 'simulation.launch')
-    def test_bearing_node(self):
-        assert True
-
