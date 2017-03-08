9cf6c79176403e890030600859f0a59e74832f84
Diptodip Deb <dr.diptodip@gmail.com>
2017-03-07 19:55:49 -0500

    Begin parsing world XML files

--- buzzmobile/simulation/models/gzb_utils/world_to_polyline.py
+++ buzzmobile/simulation/models/gzb_utils/world_to_polyline.py
@@ -7,3 +7,9 @@ def straight_road_coordinates_to_latlons(road_coordinates):
         (lat, lon) = convert(coordinate[0], coordinate[1])
         latlons.append((lat, lon))
     return latlons
+
+def world_xml_to_road_coordinates(filename):
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    for child in root:
+        print (child.tag, child.attrib)
