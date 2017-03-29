c36e491eb164c537b215601af933a1399e9157d5
Diptodip Deb <dr.diptodip@gmail.com>
2017-03-28 23:47:08 -0400

    Add camera element to XML world file

--- buzzmobile/simulation/models/gzb_utils/world_to_polyline.py
+++ buzzmobile/simulation/models/gzb_utils/world_to_polyline.py
@@ -1,7 +1,9 @@
+import sys
+import xml
 import xml.etree.ElementTree as ET
 from gzb_to_latlon import convert
 
-def straight_road_coordinates_to_latlons(road_coordinates):
+def road_coordinates_to_latlons(road_coordinates):
     latlons = []
     for coordinate in road_coordinates:
         (lat, lon) = convert(coordinate[0], coordinate[1])
@@ -12,4 +14,14 @@ def world_xml_to_road_coordinates(filename):
     tree = ET.parse(filename)
     root = tree.getroot()
     for child in root:
-        print (child.tag, child.attrib)
+        if 'world' == str(child.tag):
+            gui = xml.Element('gui')
+            camera = xml.Element('camera')
+            pose = xml.Element('pose')
+            pose.text = '0 0 50 0 0 0'
+            camera.append(pose)
+            gui.append(camera)
+            child.append(gui)
+
+
+if __name__ == '__main__': world_xml_to_road_coordinates(sys.argv[1])
